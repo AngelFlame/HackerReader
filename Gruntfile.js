48fcdce62f0e449352a6fefd58f712a2bb6734b5
EragonJ <eragonj@eragonj.me>
2014-02-07 01:06:18 +0800

    add first prototype

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,32 +10,6 @@ module.exports = function(grunt) {
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
       ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    // Task configuration.
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
-        }
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['public/js/*.js', 'test/**/*.js']
-      }
-    },
     compass: {
       dist: {
         options: {
@@ -47,14 +21,6 @@ module.exports = function(grunt) {
       options: {
         livereload: true 
       },
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test']
-      },
       compass : {
         files: 'public/scss/*.scss',
         tasks: ['compass']
@@ -63,11 +29,10 @@ module.exports = function(grunt) {
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'compass']);
+  grunt.registerTask('default', ['compass']);
 
 };
